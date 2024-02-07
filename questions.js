3b6006488e5c09adc6d3c3f0268f21c548b0afae
Lahitan <harunatawa111@gmail.com>
2024-02-07 15:35:56 +0100

    my first commit

---
+++ questions.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is CSS used for?",
+        answer: "D. For Styling",
+        options: [
+            "A. For Modification",
+             " B. For playing",
+            "C. For flyer",
+            "D. For Styling"    
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does HTML do?",
+        answer: "A. Structure a website",
+        options: [
+            "A. Structure a website",
+            "B. Build the full website",
+            "C. Making website Interactive",
+            "D. For Styling"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does SQL stand for?",
+        answer: "D. Structure Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structure Query Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does UL stand for?",
+        answer: "A. Unorderd List",
+        options: [
+             "A. Unorderd List",
+            "B. Under List",
+            "C. United Level",
+            "D. Understant List"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does OL stand for?",
+        answer: "B. Orderd List",
+        options: [
+            "A. Over List",
+            "B. Orderd List",
+            "C. Operate Level",
+            "D. Ordinary List"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is color used for in CSS?",
+        answer: "A. Change text color",
+        options: [
+            "A. Change text color",
+            "B. Chand background color",
+            "C. Color the website",
+            "D. Make color work"
+        ]
+    },
+    {
+        numb: 7,
+        question: "what is does <img> tag represent in HTML?",
+        answer: "C. To input an image in a website",
+        options: [
+             "A. To change the images in a website",
+            "B. To generate image",
+            "C. To input an image in a website",
+            "D. To suggest an image"
+        ]
+    },
+    {
+        numb: 8,
+        question: "what is the <a> tag used for?",
+        answer: "A. To input a link in a website",
+        options: [
+             "A. To input a link in a website",
+            "B. To generate a link",
+            "C. To improve a link",
+            "D. To suggest a link"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What does the <p> tag represent?",
+        answer: "C. Paragraph",
+        options: [
+             "A. People",
+            "B Purple",
+            "C. Paragraph",
+            "D. Paper"
+        ]
+    },
+    {
+        numb: 10,
+        question: "<h1> means?",
+        answer: "A. Header one",
+        options: [
+             "A. Header one",
+            "B Help one",
+            "C. Holder one",
+            "D. Help Online"
+        ]
+    } 
+]
